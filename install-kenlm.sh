23d30f92aeb3389983835268469cad7a1e4331a4
aisyahrzk <aisyahrazak171@gmail.com>
2023-09-03 14:10:21 +0000

    kenlm

---
+++ install-kenlm.sh
@@ -0,0 +1,6 @@
+sudo apt install build-essential libboost-system-dev libboost-thread-dev libboost-program-options-dev libboost-test-dev -y
+wget -O - https://kheafield.com/code/kenlm.tar.gz | tar xz
+mkdir kenlm/build
+cd kenlm/build
+cmake ..
+make -j2
